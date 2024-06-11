7018506ae5de69781102d21889eccc4f1c5bd4f1
EthanGoh1008 <S10258606@connect.np.edu.sg>
2024-06-11 17:35:09 +0800

    Shooting mechanics and movement completed
    Shooting mechanics and movement completed

---
+++ City Shield/Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private GameObject bulletPoint;
+    [SerializeField]
+    private float bulletSpeed = 600;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootBullet();
+        }
+    }
+
+    void ShootBullet()
+    {
+        Debug.Log("Shoot!");
+        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        Destroy(bullet, 3);
+    }
+}
